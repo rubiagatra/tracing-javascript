12a0d44caf68496b7b681596bbb87435bfe63377
Doni Rubiagatra <doni@zero-one-group.com>
2024-11-25 20:29:19 +0800

    feat: add manually instrument the code

--- packages/fastify-app/app.js
+++ packages/fastify-app/app.js
@@ -1,5 +1,8 @@
 import "./instrumentation.js";
 import Fastify from "fastify";
+import { trace } from "@opentelemetry/api";
+
+const tracer = trace.getTracer("fastify-app", "0.1.0");
 
 const fastify = Fastify({
   logger: false,
@@ -13,6 +16,27 @@ fastify.get("/", async function (request, reply) {
   });
   const data = await response.json();
   reply.send({ data });
+});
+
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+fastify.get("/fibonacci/:n", (request, reply) => {
+  const n = parseInt(request.params.n, 10);
+
+  const span = tracer.startSpan("calculate-fibonacci-number", {
+    attributes: {
+      "fibonacci.input": n,
+    },
+  });
+
+  const result = fibonacci(n);
+  span.setAttribute("fibonacci.result", result);
+  span.end();
+
+  reply.send({ result });
 });
 
 const PORT = parseInt(process.env.PORT || "8080");
