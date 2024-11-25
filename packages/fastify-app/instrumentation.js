486b724a136ff7235e73415ce6daed48ce14d2f0
Doni Rubiagatra <doni@zero-one-group.com>
2024-11-25 20:37:41 +0800

    feat: add jaeger backend otel collector

--- packages/fastify-app/instrumentation.js
+++ packages/fastify-app/instrumentation.js
@@ -1,9 +1,9 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter(),
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
